389a2748a029220ebb8504776be6896c2ef7ed89
Paul Nelson <ultrono@gmail.com>
2023-05-23 01:51:17 +0200

    Update generate-comments-page.js: changing back to graphQL

--- generate-comments-page.js
+++ generate-comments-page.js
@@ -4,58 +4,59 @@ const fs = require('fs');
 const owner = 'ultronozm';
 const repo = 'math';
 
-async function fetchRecentDiscussions() {
-  const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/discussions`, {
-    headers: {
-      Authorization: `token ${process.env.GITHUB_TOKEN}`,
-      Accept: 'application/vnd.github.echo-preview+json',
-    },
-    params: {
-      sort: 'updated',
-      direction: 'desc',
-      per_page: 10,
-    },
-  });
-
-  return response.data;
-}
-
-async function fetchCommentsForDiscussion(discussionNumber) {
-  const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/discussions/${discussionNumber}/comments`, {
-    headers: {
-      Authorization: `token ${process.env.GITHUB_TOKEN}`,
-      Accept: 'application/vnd.github.echo-preview+json',
-    },
-    params: {
-      per_page: 10,
-    },
-  });
-
-  return response.data;
-}
-
 async function fetchRecentComments() {
-  try {
-    const discussions = await fetchRecentDiscussions();
-    
-    for (const discussion of discussions) {
-      discussion.comments = await fetchCommentsForDiscussion(discussion.number);
-    }
+    try {
+	const response = await axios({
+	    url: 'https://api.github.com/graphql',
+	    method: 'post',
+	    headers: {
+		Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+	    },
+	    data: {
+		query: `
+          query RecentDiscussions($owner: String!, $repo: String!) {
+            repository(owner: $owner, name: $repo) {
+              discussions(first: 10, orderBy: {field: UPDATED_AT, direction: DESC}) {
+                nodes {
+                  title
+                  url
+                  comments(last: 10) {
+                    nodes {
+                      id
+                      author {
+                        login
+                      }
+                      createdAt
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+		variables: { owner, repo },
+	    },
+	});
 
-    return discussions.map(discussion => ({
-      title: discussion.title,
-      url: discussion.html_url,
-      recentComments: discussion.comments.slice(-10).map(comment => ({
-        id: comment.id,
-        url: `${discussion.html_url}#discussioncomment-${comment.id}`,
-        author: comment.user.login,
-        createdAt: comment.created_at,
-      })),
-    }));
-  } catch (err) {
-    console.error('Error while fetching discussions or comments:', err);
-    return [];
-  }
+	if (response.data.errors) {
+	    console.error('Error while fetching discussions:', response.data.errors);
+	    return [];
+	}
+	
+	return response.data.data.repository.discussions.nodes.map(discussion => ({
+	    title: discussion.title,
+	    url: discussion.url,
+	    recentComments: discussion.comments.nodes.map(comment => ({
+		id: comment.id,
+		url: `${discussion.url}#discussioncomment-${comment.id}`,
+		author: comment.author.login,
+		createdAt: comment.createdAt,
+	    })),
+	}));
+    } catch (err) {
+	console.error('Error while executing GraphQL query:', err);
+	return [];
+    }
 }
 
 fetchRecentComments().then(comments => {
@@ -64,72 +65,3 @@ fetchRecentComments().then(comments => {
   console.error(err);
   process.exit(1);
 });
-
-
-// const axios = require('axios');
-// const fs = require('fs');
-
-// const owner = 'ultronozm';
-// const repo = 'math';
-
-// async function fetchRecentComments() {
-//     try {
-// 	const response = await axios({
-// 	    url: 'https://api.github.com/graphql',
-// 	    method: 'post',
-// 	    headers: {
-// 		Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-// 	    },
-// 	    data: {
-// 		query: `
-//           query RecentDiscussions($owner: String!, $repo: String!) {
-//             repository(owner: $owner, name: $repo) {
-//               discussions(first: 10, orderBy: {field: UPDATED_AT, direction: DESC}) {
-//                 nodes {
-//                   title
-//                   url
-//                   comments(last: 10) {
-//                     nodes {
-//                       id
-//                       author {
-//                         login
-//                       }
-//                       createdAt
-//                     }
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         `,
-// 		variables: { owner, repo },
-// 	    },
-// 	});
-
-// 	if (response.data.errors) {
-// 	    console.error('Error while fetching discussions:', response.data.errors);
-// 	    return [];
-// 	}
-	
-// 	return response.data.data.repository.discussions.nodes.map(discussion => ({
-// 	    title: discussion.title,
-// 	    url: discussion.url,
-// 	    recentComments: discussion.comments.nodes.map(comment => ({
-// 		id: comment.id,
-// 		url: `${discussion.url}#discussioncomment-${comment.id}`,
-// 		author: comment.author.login,
-// 		createdAt: comment.createdAt,
-// 	    })),
-// 	}));
-//     } catch (err) {
-// 	console.error('Error while executing GraphQL query:', err);
-// 	return [];
-//     }
-// }
-
-// fetchRecentComments().then(comments => {
-//   fs.writeFileSync('recent-discussions.json', JSON.stringify(comments, null, 2));
-// }).catch(err => {
-//   console.error(err);
-//   process.exit(1);
-// });
