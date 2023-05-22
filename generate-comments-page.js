a07bada000666d12e79ec5b9690cb13f50a5768a
Paul Nelson <ultrono@gmail.com>
2023-05-22 21:31:31 +0200

    recent comments tweaking

--- generate-comments-page.js
+++ generate-comments-page.js
@@ -1,73 +1,29 @@
 const axios = require('axios');
 const fs = require('fs');
 
-// Replace with your repository's owner and name
 const owner = 'ultronozm';
 const repo = 'math';
+const numComments = 5;
 
-// GraphQL query to fetch recent discussions
-const query = `
-  query RecentDiscussions($owner: String!, $repo: String!) {
-    repository(owner: $owner, name: $repo) {
-      discussions(first: 10, orderBy: {field: UPDATED_AT, direction: DESC}) {
-        nodes {
-          title
-          url
-        }
-      }
-    }
-  }
-`;
-
-// Function to fetch recent discussions from the GitHub API
-async function fetchRecentDiscussions() {
-  const response = await axios({
-    url: 'https://api.github.com/graphql',
-    method: 'post',
+const fetchRecentDiscussions = async () => {
+  const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/discussions`, {
     headers: {
-      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-    },
-    data: {
-      query,
-      variables: { owner, repo },
-    },
+      Accept: 'application/vnd.github.squirrel-girl-preview+json',
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`
+    }
   });
 
-  if (response.data.errors) {
-    console.error(response.data.errors);
-    throw new Error('Failed to fetch recent discussions');
-  }
+  const discussions = response.data.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
 
-  return response.data.data.repository.discussions.nodes;
-}
+  return discussions.slice(0, numComments).map(discussion => ({
+    title: discussion.title,
+    url: discussion.html_url,
+    lastCommentedBy: discussion.comments.nodes[0].user.login,
+    lastCommentedAt: discussion.comments.nodes[0].updated_at,
+    commentText: discussion.comments.nodes[0].body
+  }));
+};
 
-// Function to generate an HTML page with a list of discussions
-function generateHtml(discussions) {
-  return `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Recent Discussions</title>
-    </head>
-    <body>
-      <h1>Recent Discussions</h1>
-      <ul>
-        ${discussions.map(discussion => `
-          <li>
-            <a href="${discussion.url}">${discussion.title}</a>
-          </li>
-        `).join('')}
-      </ul>
-    </body>
-    </html>
-  `;
-}
-
-// Fetch recent discussions and generate an HTML page
-fetchRecentDiscussions().then(discussions => {
-  const html = generateHtml(discussions);
-  fs.writeFileSync('recent-discussions.html', html);
-}).catch(err => {
-  console.error(err);
-  process.exit(1);
+fetchRecentDiscussions().then((discussions) => {
+  fs.writeFileSync('recent-discussions.json', JSON.stringify(discussions, null, 2));
 });
