4627a928df3330552926e8f918694ac3bff7d3a9
Paul Nelson <ultrono@gmail.com>
2023-05-26 14:50:59 +0200

    Create convert.sh

---
+++ convert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+file=$1
+converted_file="${file%.tex}.html"
+
+echo "Converting $file to html"
+
+if command -v docker &> /dev/null
+then
+    docker run --volume "`pwd`:`pwd`" --workdir "`pwd`" pandoc/core:3.1 --standalone "$file" -o "$converted_file" --mathjax --citeproc
+    sudo chown runner:docker "$converted_file"
+else
+    pandoc --standalone "$file" -o "$converted_file" --mathjax --citeproc
+fi
+
+emacs --batch "$converted_file" -l ./tex2html.el --eval "(progn (tex2html-add-style-to-html-head) (tex2html-postprocess-html-buffer) (tex2html-add-comment-script) (tex2html-add-tex-pdf-links))" -f save-buffer
