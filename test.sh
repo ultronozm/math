5623064264ea8b48599c09b49204b01651f68b68
Paul Nelson <ultrono@gmail.com>
2023-05-27 08:43:35 +0200

    test

--- test.sh
+++ test.sh
@@ -4,8 +4,10 @@ temp_file=$(mktemp)
 
 cat > "$temp_file"
 
-# Run Emacs in batch mode, executing the `test` function on stdin
-emacs --batch "$temp_file" -l ./test.el --eval "(test)" -f save-buffer
+echo $(pwd)
+
+# # Run Emacs in batch mode, executing the `test` function on stdin
+emacs --batch -l test.el $temp_file --eval "(test)" -f save-buffer
 
 cat "$temp_file"
 
