7410098dc95ae8e16becabb665e3011ff3ff8843
Paul Nelson <ultrono@gmail.com>
2023-06-23 07:17:19 +0200

    added --toc to pandoc calls

--- offline-convert.sh
+++ offline-convert.sh
@@ -7,5 +7,5 @@ echo "Converting $file to html"
 echo "file: $file"
 echo "converted_file: $converted_file"
 
-pandoc --standalone "$file" -o "$converted_file" --mathjax --citeproc
+pandoc --standalone "$file" -o "$converted_file" --mathjax --citeproc --toc
 emacs --batch "$converted_file" -l ./tex2html.el --eval "(progn (tex2html-add-style-to-html-head) (tex2html-postprocess-html-buffer) (tex2html-add-comment-script) (tex2html-add-tex-pdf-links))" -f save-buffer
